006e555bc4021ad295eaf7223a2ec99b1d164447
Huachao Mao <huachaomao@gmail.com>
2016-12-31 20:22:33 +0800

    refactor response store class

--- src/responseStore.ts
+++ src/responseStore.ts
@@ -3,13 +3,21 @@
 import { HttpResponse } from './models/httpResponse';
 
 export class ResponseStore {
-    private static cache: { [key: string]: HttpResponse } = {};
+    private static cache: Map<string, HttpResponse> = new Map<string, HttpResponse>();
+    private static lastResponseUri: string = null;
 
     static add(uri: string, response: HttpResponse) {
-        ResponseStore.cache[uri] = response;
+        ResponseStore.cache.set(uri, response);
+        ResponseStore.lastResponseUri = uri;
     }
 
     static get(uri: string): HttpResponse {
-        return ResponseStore.cache[uri];
+        return ResponseStore.cache.get(uri);
+    }
+
+    static getLatestResponse(): HttpResponse {
+        return ResponseStore.lastResponseUri !== null
+                ? ResponseStore.get(ResponseStore.lastResponseUri)
+                : null;
     }
 }
