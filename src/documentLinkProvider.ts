ea1f8de3202628f3ec5d8f7e42902d555f10c89d
Huachao Mao <huachaomao@gmail.com>
2017-05-02 16:57:31 +0800

    Add request body document link provider

---
+++ src/documentLinkProvider.ts
@@ -0,0 +1,52 @@
+'use strict';
+
+import { DocumentLink, DocumentLinkProvider, TextDocument, Range, Position, Uri, workspace, CancellationToken } from 'vscode';
+import * as path from 'path';
+
+export class RequestBodyDocumentLinkProvider implements DocumentLinkProvider {
+
+    private _linkPattern = /^(\<\s+)(\S+)(\s*)$/g;
+
+    public provideDocumentLinks(document: TextDocument, _token: CancellationToken): DocumentLink[] {
+        const results: DocumentLink[] = [];
+        const base = path.dirname(document.uri.fsPath);
+        const text = document.getText();
+
+        let lines: string[] = text.split(/\r?\n/g);
+        for (var index = 0; index < lines.length; index++) {
+            var line = lines[index];
+            let match: RegExpMatchArray | null
+            if (match = this._linkPattern.exec(line)) {
+                let filePath = match[2];
+                const offset = match[1].length;
+                const linkStart = new Position(index, offset);
+                const linkEnd = new Position(index, offset + filePath.length);
+                results.push(new DocumentLink(
+                    new Range(linkStart, linkEnd),
+                    this.normalizeLink(document, filePath, base)
+                ));
+            }
+        }
+
+        return results;
+    }
+
+    private normalizeLink(document: TextDocument, link: string, base: string): Uri {
+        const uri = Uri.parse(link);
+        if (uri.scheme) {
+            return uri;
+        }
+
+        // assume it must be a file
+        let resourcePath;
+        if (!uri.path) {
+            resourcePath = document.uri.path;
+        } else if (uri.path[0] === '/') {
+            resourcePath = path.join(workspace.rootPath || '', uri.path);
+        } else {
+            resourcePath = path.join(base, uri.path);
+        }
+
+        return Uri.parse(`command:rest-client._openDocumentLink?${encodeURIComponent(JSON.stringify({ fragment: uri.fragment, path: resourcePath }))}`);
+    }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { ExtensionContext, commands, languages, TextDocument, Range } from 'vscode';
+import { ExtensionContext, commands, languages, TextDocument, Range, Uri, workspace, window } from 'vscode';
 import { RequestController } from './controllers/requestController';
 import { HistoryController } from './controllers/historyController';
 import { ResponseController } from './controllers/responseController';
@@ -10,6 +10,7 @@ import { EnvironmentController } from './controllers/environmentController';
 import { HttpCompletionItemProvider } from './httpCompletionItemProvider';
 import { CustomVariableHoverProvider } from './customVariableHoverProvider';
 import { HttpCodeLensProvider } from './httpCodeLensProvider';
+import { RequestBodyDocumentLinkProvider } from './documentLinkProvider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -38,9 +39,13 @@ export async function activate(context: ExtensionContext) {
     context.subscriptions.push(commands.registerCommand('rest-client.generate-codesnippet', () => codeSnippetController.run()));
     context.subscriptions.push(commands.registerCommand('rest-client.copy-codesnippet', () => codeSnippetController.copy()));
     context.subscriptions.push(commands.registerCommand('rest-client.switch-environment', () => environmentController.switchEnvironment()));
+    context.subscriptions.push(commands.registerCommand('rest-client._openDocumentLink', args => {
+        workspace.openTextDocument(Uri.file(args.path)).then(window.showTextDocument, window.showErrorMessage);
+    }));
     context.subscriptions.push(languages.registerCompletionItemProvider('http', new HttpCompletionItemProvider()));
-    context.subscriptions.push(languages.registerHoverProvider('http', new CustomVariableHoverProvider()))
-    context.subscriptions.push(languages.registerCodeLensProvider('http', new HttpCodeLensProvider()))
+    context.subscriptions.push(languages.registerHoverProvider('http', new CustomVariableHoverProvider()));
+    context.subscriptions.push(languages.registerCodeLensProvider('http', new HttpCodeLensProvider()));
+    context.subscriptions.push(languages.registerDocumentLinkProvider('http', new RequestBodyDocumentLinkProvider()))
 }
 
 // this method is called when your extension is deactivated
