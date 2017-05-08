4758e4f9d9a51ed021b0eaffa4af43b6a93a8b86
Huachao Mao <huachaomao@gmail.com>
2017-05-08 15:57:10 +0800

    Improve the performance of rendering large response #59 #94

--- src/views/httpResponseTextDocumentContentProvider.ts
+++ src/views/httpResponseTextDocumentContentProvider.ts
@@ -3,6 +3,7 @@
 import { Uri, extensions } from 'vscode';
 import { BaseTextDocumentContentProvider } from './baseTextDocumentContentProvider';
 import { RestClientSettings } from '../models/configurationSettings';
+import { HttpResponse } from "../models/httpResponse";
 import { MimeUtility } from '../mimeUtility';
 import { ResponseFormatUtility } from '../responseFormatUtility';
 import { ResponseStore } from '../responseStore';
@@ -10,7 +11,6 @@ import * as Constants from '../constants';
 import * as path from 'path';
 
 const hljs = require('highlight.js');
-const codeHighlightLinenums = require('code-highlight-linenums');
 
 var autoLinker = require('autolinker');
 
@@ -34,11 +34,9 @@ export class HttpResponseTextDocumentContentProvider extends BaseTextDocumentCon
                 if (contentType && MimeUtility.isBrowserSupportedImageFormat(contentType)) {
                     innerHtml = `<img src="data:${contentType};base64,${new Buffer(response.bodyStream).toString('base64')}">`;
                 } else {
-                    let code = `HTTP/${response.httpVersion} ${response.statusCode} ${response.statusMessage}
-${HttpResponseTextDocumentContentProvider.formatHeaders(response.headers)}
-${ResponseFormatUtility.FormatBody(response.body, response.getResponseHeaderValue("content-type"))}`;
+                    let code = this.highlightResponse(response);
                     width = (code.split(/\r\n|\r|\n/).length + 1).toString().length;
-                    innerHtml = `<pre><code class="http">${codeHighlightLinenums(code, { hljs: hljs, lang: 'http', start: 1 })}</code></pre>`;
+                    innerHtml = `<pre><code>${this.addLineNums(code)}</code></pre>`
                 }
                 return `
             <head>
@@ -53,6 +51,22 @@ ${ResponseFormatUtility.FormatBody(response.body, response.getResponseHeaderValu
             </body>`;
             }
         }
+    }
+
+    private highlightResponse(response: HttpResponse): string {
+        let code = '';
+        let nonBodyPart = `HTTP/${response.httpVersion} ${response.statusCode} ${response.statusMessage}
+${HttpResponseTextDocumentContentProvider.formatHeaders(response.headers)}`;
+        code += hljs.highlight('http', nonBodyPart + '\r\n').value;
+        let contentType = response.getResponseHeaderValue("content-type");
+        let bodyPart = `${ResponseFormatUtility.FormatBody(response.body, contentType)}`;
+        let bodyLanguageAlias = HttpResponseTextDocumentContentProvider.getHightLanguageAlias(contentType);
+        if (bodyLanguageAlias) {
+            code += hljs.highlight(bodyLanguageAlias, bodyPart).value;
+        } else {
+            code += hljs.highlightAuto(bodyPart).value;
+        }
+        return code;
     }
 
     private getSettingsOverrideStyles(width: number): string {
@@ -71,6 +85,45 @@ ${ResponseFormatUtility.FormatBody(response.body, response.getResponseHeaderValu
             `margin-left: calc(-${width}ch + -27px );`,
             '}',
             '</style>'].join('\n');
+    }
+
+    private addLineNums(code): string {
+        code = code.replace(/([\r\n]\s*)(<\/span>)/ig, '$2$1');
+
+        code = this.cleanLineBreaks(code);
+
+        code = code.split(/\r\n|\r|\n/);
+        let max = (1 + code.length).toString().length;
+
+        code = code
+            .map(function(line, i) {
+                return '<span class="line width-' + max + '" start="' + (1 + i) + '">' + line + '</span>';
+            })
+            .join('\n');
+        return code;
+    }
+
+    private cleanLineBreaks(code): string {
+        let openSpans = [],
+            matcher = /<\/?span[^>]*>|\r\n|\r|\n/ig,
+            newline = /\r\n|\r|\n/,
+            closingTag = /^<\//;
+
+        return code.replace(matcher, function(match) {
+            if(newline.test(match)) {
+                if(openSpans.length) {
+                    return openSpans.map(function() { return '</span>' }).join('') + match + openSpans.join('');
+                } else {
+                    return match;
+                }
+            } else if(closingTag.test(match)) {
+                openSpans.pop();
+                return match;
+            } else {
+                openSpans.push(match);
+                return match;
+            }
+        });
     }
 
     private addUrlLinks(innerHtml: string) {
@@ -99,5 +152,23 @@ ${ResponseFormatUtility.FormatBody(response.body, response.getResponseHeaderValu
             }
         }
         return headerString;
+    }
+
+    private static getHightLanguageAlias(contentType: string): string {
+        contentType = contentType.toLowerCase();
+        let mime = MimeUtility.parse(contentType);
+        let type = mime.type;
+        let suffix = mime.suffix;
+        if (type === 'application/json' || suffix === '+json') {
+            return 'json';
+        } else if (type === 'application/javascript') {
+            return 'javascript';
+        } else if (type === 'application/xml' || type === 'text/xml' || suffix === '+xml') {
+            return 'xml';
+        } else if (type === 'text/html') {
+            return 'html';
+        } else {
+            return null;
+        }
     }
 }
