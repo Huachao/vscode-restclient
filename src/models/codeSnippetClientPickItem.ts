488f8e6404d3306ccd249275cf2124bf175b2bdf
Huachao Mao <huachaomao@gmail.com>
2017-07-06 14:23:09 +0800

    Increase to 4 spaces indent

--- src/models/codeSnippetClientPickItem.ts
+++ src/models/codeSnippetClientPickItem.ts
@@ -4,8 +4,8 @@ import { QuickPickItem } from 'vscode';
 import { CodeSnippetClient } from './codeSnippetClient';
 
 export class CodeSnippetClientQuickPickItem implements QuickPickItem {
- public label: string;
- public description: string;
- public detail: string;
- public rawClient: CodeSnippetClient;
+    public label: string;
+    public description: string;
+    public detail: string;
+    public rawClient: CodeSnippetClient;
 }
