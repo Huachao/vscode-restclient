5403e55ed6d01058ef3c829cd4984bb3551b4154
Michael Flanakin <micflan@microsoft.com>
2018-01-15 01:39:13 -0800

    PR feedback 3

--- src/variableProcessor.ts
+++ src/variableProcessor.ts
@@ -216,7 +216,7 @@ export class VariableProcessor {
                                                 : displayName;
                                             let bestMatches = [];
                                             const bestMatchesRegex = new RegExp(`(^${displayNameFirstWord}\.com$)|(^${displayNameFirstWord}\.[a-z]+(?:\.[a-z]+)?$)|(^${displayNameFirstWord}[a-z]+\.com$)|(^${displayNameFirstWord}[^:]*$)|(^[^:]*${displayNameFirstWord}[^:]*$)`, "gi");
-                                            const bestMatchesRegexGroups = (bestMatchesRegex.source.match(new RegExp(`${displayNameFirstWord}`, "g")) || []).length;
+                                            const bestMatchesRegexGroups = (bestMatchesRegex.source.match(new RegExp(`${displayNameFirstWord}`, "g"))).length;
                                             for (let d of element.domains) {
                                                 // find matches; use empty array for all captures (+1 for the full string) if no matches found
                                                 const matches = bestMatchesRegex.exec(d)
