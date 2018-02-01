9e66ddc11b8dc876add0c74800f3a059dfb1d8dc
Christian Brevik <christian.brevik@gmail.com>
2018-02-01 18:30:29 +0100

    Make cache value required

--- src/models/requestVariableCacheValue.ts
+++ src/models/requestVariableCacheValue.ts
@@ -6,6 +6,6 @@ import { HttpResponse } from "./httpResponse";
 export class RequestVariableCacheValue {
     public constructor(
         public request: HttpRequest,
-        public response?: HttpResponse) {
+        public response: HttpResponse) {
     }
 }
