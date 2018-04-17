d0152adeb8aba46c1e9d527a27218717defe89ed
Huachao Mao <huachaomao@gmail.com>
2018-04-17 17:28:30 +0800

    Refactor telemetry properties parameter type

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -1,6 +1,5 @@
 'use strict';
 
-import { Headers } from './models/base';
 import { RestClientSettings } from './models/configurationSettings';
 import * as Constants from './constants';
 import * as appInsights from "applicationinsights";
@@ -12,7 +11,7 @@ appInsights.setup(Constants.AiKey)
 export class Telemetry {
     private static readonly restClientSettings: RestClientSettings = new RestClientSettings();
 
-    public static sendEvent(eventName: string, properties?: Headers) {
+    public static sendEvent(eventName: string, properties?: { [key: string]: string }) {
         try {
             if (Telemetry.restClientSettings.enableTelemetry) {
                 appInsights.defaultClient.trackEvent({name: eventName, properties});
