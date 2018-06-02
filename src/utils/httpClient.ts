df64dbe2dfeb468ca2fb21c8db31d881c7d80608
Germán Méndez Bravo <german.mb@gmail.com>
2018-06-02 17:45:00 -0500

    Do not add Host header if url already has one (#215)
    * Do not add Host header if url already has one
    
    While doing `GET http://some.domain` and having a default `Host` header,
    it'd add a wrong Host header which wouldn't work for the request in progress.
    
    * Merge if() and use lowercase to compare header

--- src/utils/httpClient.ts
+++ src/utils/httpClient.ts
@@ -83,7 +83,7 @@ export class HttpClient {
 
         // add default headers if not specified
         for (let header in this._settings.defaultHeaders) {
-            if (!hasHeader(options.headers, header)) {
+            if (!hasHeader(options.headers, header) && (header.toLowerCase() !== 'host' || httpRequest.url[0] === '/')) {
                 const value = this._settings.defaultHeaders[header];
                 if (value) {
                     options.headers[header] = value;
