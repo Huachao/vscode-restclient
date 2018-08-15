25ecf7150e8dcd6ffff90794c6695c0d171fbd82
Yehuda Graber <yehuda@dv01.co>
2018-08-15 18:06:32 -0400

    oops (tslint)

--- src/utils/responseFormatUtility.ts
+++ src/utils/responseFormatUtility.ts
@@ -3,14 +3,14 @@
 import { window } from 'vscode';
 import { MimeUtility } from './mimeUtility';
 const pd = require('pretty-data').pd;
-const beautify = require('js-beautify').js_beautify
+const beautify = require('js-beautify').js_beautify;
 
 export class ResponseFormatUtility {
     public static formatBody(body: string, contentType: string, suppressValidation: boolean): string {
         if (contentType) {
             if (MimeUtility.isJSON(contentType)) {
                 if (ResponseFormatUtility.IsJsonString(body)) {
-                    body = beautify(body, { indent_size: 4 })
+                    body = beautify(body, { indent_size: 4 });
                 } else if (!suppressValidation) {
                     window.showWarningMessage('The content type of response is application/json, while response body is not a valid json string');
                 }
