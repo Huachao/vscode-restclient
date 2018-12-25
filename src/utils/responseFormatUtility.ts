d604993535594d53e08a7731921c6d25cb624d53
Huachao Mao <huachaomao@gmail.com>
2018-12-25 14:03:48 +0800

    Implement the JSON prettify logic on our own based on jsonc-parser #301

--- src/utils/responseFormatUtility.ts
+++ src/utils/responseFormatUtility.ts
@@ -1,17 +1,32 @@
 'use strict';
 
+import { createScanner, SyntaxKind } from 'jsonc-parser';
+import * as os from 'os';
 import { window } from 'vscode';
 import { MimeUtility } from './mimeUtility';
 import { isJSONString } from './misc';
 const pd = require('pretty-data').pd;
-const beautify = require('js-beautify').js_beautify;
+
 
 export class ResponseFormatUtility {
+
+    private static readonly specialTokenMapping = {
+        [SyntaxKind.OpenBraceToken]: '{',
+        [SyntaxKind.CloseBraceToken]: '}',
+        [SyntaxKind.OpenBracketToken]: '[',
+        [SyntaxKind.CloseBracketToken]: ']',
+        [SyntaxKind.ColonToken]: ':',
+        [SyntaxKind.CommaToken]: ',',
+        [SyntaxKind.NullKeyword]: 'null',
+        [SyntaxKind.TrueKeyword]: 'true',
+        [SyntaxKind.FalseKeyword]: 'false'
+    };
+
     public static formatBody(body: string, contentType: string, suppressValidation: boolean): string {
         if (contentType) {
             if (MimeUtility.isJSON(contentType)) {
                 if (isJSONString(body)) {
-                    body = beautify(body, { indent_size: 2 });
+                    body = ResponseFormatUtility.jsonPrettify(body);
                 } else if (!suppressValidation) {
                     window.showWarningMessage('The content type of response is application/json, while response body is not a valid json string');
                 }
@@ -22,11 +37,98 @@ export class ResponseFormatUtility {
             } else {
                 // Add this for the case that the content type of response body is not very accurate #239
                 if (isJSONString(body)) {
-                    body = beautify(body, { indent_size: 2 });
+                    body = ResponseFormatUtility.jsonPrettify(body);
                 }
             }
         }
 
         return body;
+    }
+
+    private static jsonPrettify(text: string, indentSize = 2) {
+        const scanner = createScanner(text, true);
+
+        let indentLevel = 0;
+
+        function newLineAndIndent() {
+            return os.EOL + ' '.repeat(indentLevel * indentSize);
+        }
+
+        function getTokenValue() {
+            const offset = scanner.getTokenOffset();
+            const length = scanner.getTokenLength();
+            return text.substr(offset, length);
+        }
+
+
+        let firstToken = scanner.scan();
+        let firstTokenValue = getTokenValue();
+        let secondToken: SyntaxKind;
+        let secondTokenValue: string;
+        let result = '';
+
+
+        while (firstToken !== SyntaxKind.EOF) {
+            secondToken = scanner.scan();
+            secondTokenValue = getTokenValue();
+
+
+            switch (firstToken) {
+                case SyntaxKind.OpenBraceToken:
+                    result += ResponseFormatUtility.specialTokenMapping[firstToken];
+                    if (secondToken !== SyntaxKind.CloseBraceToken) {
+                        indentLevel++;
+                        result += newLineAndIndent();
+                    }
+                    break;
+                case SyntaxKind.OpenBracketToken:
+                    result += ResponseFormatUtility.specialTokenMapping[firstToken];
+                    if (secondToken !== SyntaxKind.CloseBracketToken) {
+                        indentLevel++;
+                        result += newLineAndIndent();
+                    }
+                    break;
+                case SyntaxKind.CloseBraceToken:
+                case SyntaxKind.CloseBracketToken:
+                case SyntaxKind.NullKeyword:
+                case SyntaxKind.TrueKeyword:
+                case SyntaxKind.FalseKeyword:
+                    result += ResponseFormatUtility.specialTokenMapping[firstToken];
+                    if (secondToken === SyntaxKind.CloseBraceToken
+                        || secondToken === SyntaxKind.CloseBracketToken) {
+                        indentLevel--;
+                        result += newLineAndIndent();
+                    }
+                    break;
+                case SyntaxKind.CommaToken:
+                    result += ResponseFormatUtility.specialTokenMapping[firstToken];
+                    if (secondToken === SyntaxKind.CloseBraceToken
+                        || secondToken === SyntaxKind.CloseBracketToken) {
+                        indentLevel--;
+                    }
+                    result += newLineAndIndent();
+                    break;
+                case SyntaxKind.ColonToken:
+                    result += ResponseFormatUtility.specialTokenMapping[firstToken] + ' ';
+                    break;
+                case SyntaxKind.StringLiteral:
+                case SyntaxKind.NumericLiteral:
+                case SyntaxKind.Unknown:
+                    result += firstTokenValue;
+                    if (secondToken === SyntaxKind.CloseBraceToken
+                        || secondToken === SyntaxKind.CloseBracketToken) {
+                        indentLevel--;
+                        result += newLineAndIndent();
+                    }
+                    break;
+                default:
+                    result += firstTokenValue;
+            }
+
+            firstToken = secondToken;
+            firstTokenValue = secondTokenValue;
+        }
+
+        return result;
     }
 }
