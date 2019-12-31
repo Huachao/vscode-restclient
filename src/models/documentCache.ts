edc0a9f44a6a86ba5114dcb093eeb9a0fce92e23
Huachao Mao <huachaomao@gmail.com>
2019-12-31 10:14:45 +0800

    Only cache one item for given document

--- src/models/documentCache.ts
+++ src/models/documentCache.ts
@@ -1,23 +1,29 @@
 import { TextDocument } from 'vscode';
 
+interface CacheValue<T> {
+    version: number;
+    value: T;
+}
+
 export class DocumentCache<T> {
-    private _cache: Map<string, T>;
+    private _cache: Map<string, CacheValue<T>>;
 
     public constructor() {
-        this._cache = new Map<string, T>();
+        this._cache = new Map<string, CacheValue<T>>();
     }
 
     public get(document: TextDocument): T | undefined {
-        return this._cache.get(this.getKey(document));
+        const result = this._cache.get(this.getKey(document));
+        return result?.version === document.version ? result.value : undefined;
     }
 
     public set(document: TextDocument, value: T): this {
-        this._cache.set(this.getKey(document), value);
+        this._cache.set(this.getKey(document), { version: document.version, value });
         return this;
     }
 
     public delete(document: TextDocument): boolean {
-        return this._cache.delete(this.getKey(document));
+        return this.has(document) && this._cache.delete(this.getKey(document));
     }
 
     public clear(): void {
@@ -25,10 +31,12 @@ export class DocumentCache<T> {
     }
 
     public has(document: TextDocument): boolean {
-        return this._cache.has(this.getKey(document));
+        const key = this.getKey(document);
+        return this._cache.has(key)
+            && this._cache.get(key)!.version === document.version;
     }
 
     private getKey(document: TextDocument): string {
-        return `${document.uri.toString()}@${document.version}`;
+        return `${document.uri.toString()}`;
     }
 }
