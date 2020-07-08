9b1877120b3828c812ad48f4fd7a0d631bca7dc9
ricmrodrigues <ricmrodrigues@gmail.com>
2020-07-08 10:46:53 +0100

    processing variables from external file (#632)
    * processing variables from external file
    
    * adding syntax so that external file variable parsing is optional and flagged when referencing the file via an @ sign '<@' and optionally the encoding name (utf8 by default) '<@latin1'
    
    * fixed tslint issue
    
    * updated link for file clicking
    
    * fixed regex to not support encoding without the @

--- src/utils/httpRequestParser.ts
+++ src/utils/httpRequestParser.ts
@@ -8,6 +8,7 @@ import { RequestParser } from '../models/requestParser';
 import { MimeUtility } from './mimeUtility';
 import { getContentType, getHeader, removeHeader } from './misc';
 import { parseRequestHeaders, resolveRequestBodyPath } from './requestParserUtil';
+import { VariableProcessor } from "./variableProcessor";
 
 const CombinedStream = require('combined-stream');
 const encodeurl = require('encodeurl');
@@ -21,7 +22,8 @@ enum ParseState {
 export class HttpRequestParser implements RequestParser {
     private readonly defaultMethod = 'GET';
     private readonly queryStringLinePrefix = /^\s*[&\?]/;
-    private readonly inputFileSyntax = /^<\s+(.+?)\s*$/;
+    private readonly inputFileSyntax = /^<(?:(?<processVariables>@)(?<encoding>\w+)?)?\s+(?<filepath>.+?)\s*$/;
+    private readonly defaultFileEncoding = 'utf8';
 
     public constructor(private readonly requestRawText: string, private readonly settings: RestClientSettings) {
     }
@@ -177,11 +179,19 @@ export class HttpRequestParser implements RequestParser {
             for (const [index, line] of lines.entries()) {
                 if (this.inputFileSyntax.test(line)) {
                     const groups = this.inputFileSyntax.exec(line);
-                    if (groups?.length === 2) {
-                        const inputFilePath = groups[1];
+                    const groupsValues = groups?.groups;
+                    if (groups?.length === 4 && !!groupsValues) {
+                        const inputFilePath = groupsValues.filepath;
                         const fileAbsolutePath = await resolveRequestBodyPath(inputFilePath);
                         if (fileAbsolutePath) {
-                            combinedStream.append(fs.createReadStream(fileAbsolutePath));
+                            if (groupsValues.processVariables) {
+                                const buffer = await fs.readFile(fileAbsolutePath);
+                                const fileContent = buffer.toString(groupsValues.encoding || this.defaultFileEncoding);
+                                const resolvedContent = await VariableProcessor.processRawRequest(fileContent);
+                                combinedStream.append(resolvedContent);
+                            } else {
+                                combinedStream.append(fs.createReadStream(fileAbsolutePath));
+                            }
                         } else {
                             combinedStream.append(line);
                         }
