020e517e7aae782b00382fe86baa271d5bc1d16d
Robert Szachnowski <rszachnowski@wi.ps.pl>
2020-08-24 22:31:51 +0200

    Add option handler for postman

--- src/controllers/importController.ts
+++ src/controllers/importController.ts
@@ -1,5 +1,6 @@
-import { CancellationToken, window, Uri } from 'vscode';
+import { window, workspace } from 'vscode';
 import { trace } from '../utils/decorator';
+import { PostmanImporter } from './../utils/importers/PostmanCollectionImporter';
 
 export class ImportController {
 
@@ -18,7 +19,17 @@ export class ImportController {
         if (option === undefined) {
             window.showErrorMessage("Options was not found.");
         }
-        
+
+        if (option === 'Postman') {
+            const fileContent = await workspace.fs.readFile(<Uri>file);
+            const jsonObj = JSON.parse(fileContent.toString());
+
+            const importer = new PostmanImporter();
+            const parsedContent = importer.import(jsonObj);
+            const document = await workspace.openTextDocument({ language: 'http', content: parsedContent });
+            await window.showTextDocument(document, { preview: false });
+        }
+
         /* 
         1. resolve importer based on option.
         2. map content from imported file to text in specific format.
